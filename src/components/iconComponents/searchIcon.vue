13bb232c8f4c24e172750d6a270e6b50a915654b
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-03 11:26:54 +0200

    Created the search icon

---
+++ src/components/iconComponents/searchIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <span class="material-symbols-outlined">search</span>
+</template>
+
+<style scoped>
+.material-symbols-outlined {
+  color: black;
+  font-size: 30px;
+  user-select: none;
+}
+</style>
