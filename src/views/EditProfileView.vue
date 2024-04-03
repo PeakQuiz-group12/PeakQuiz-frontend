cdf49542c9ca59b191357674af241ea80b870aaf
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2024-04-03 18:16:09 +0200

    Created EditProfileView.vue

---
+++ src/views/EditProfileView.vue
@@ -0,0 +1,22 @@
+<template>
+  <accountSettings :userImg="profileImg" />
+</template>
+
+<script>
+import AccountSettings from "@/components/accountSettings";
+import profileImage from "@/assets/__test__/ProfilePageAssets/profileImage.png";
+import { ref } from "vue";
+
+export default {
+  name: "EditProfile",
+  components: {
+    AccountSettings
+  },
+  setup() {
+    const profileImg = ref(profileImage);
+    return {
+      profileImg
+    };
+  }
+}
+</script>
