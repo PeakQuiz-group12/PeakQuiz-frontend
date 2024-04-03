bc6f4823da08b3f4ba48a334e236313551c0af59
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2024-04-03 17:47:52 +0200

    Crated ProfileView.vue

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,51 @@
+<template>
+  <ProfileHeader
+      :user-image="userImage"
+      :user-name="userName"
+      :user-title="userTitle"
+      :completed="completed"
+      :rating="rating"
+      :quizzes="quizzes"
+  />
+  <CompletedQuizes
+      :title="CompletedQuizList.title"
+      :is-completed="CompletedQuizList.isCompleted"/>
+  <ProfileRating/>
+  <CompletedQuizes
+      :title="MyQuizList.title"
+      :is-completed="MyQuizList.isCompleted"/>
+</template>
+
+<script>
+import ProfileHeader from '@/components/ProfileHeader.vue';
+import ProfilePageQuizzes from "@/components/ProfilePageQuizzes.vue";
+import ProfileRating from "@/components/ProfileRating";
+import logo from '@/assets/__test__/ProfilePageAssets/logo.png';
+
+export default {
+  name: 'profileView',
+  components: {
+    ProfileHeader,
+    ProfilePageQuizzes,
+    ProfileRating
+  },
+  data() {
+    return {
+      userImage: logo,
+      userName: 'Quiz Master',
+      userTitle: 'Trivia Titan',
+      completed: 11,
+      rating: 42,
+      quizzes: 10,
+      CompletedQuizList: {
+        title: 'Completed',
+        isCompleted: true,
+      },
+      MyQuizList: {
+        title: 'My Quiz List',
+        isCompleted: false,
+      }
+    };
+  }
+}
+</script>
