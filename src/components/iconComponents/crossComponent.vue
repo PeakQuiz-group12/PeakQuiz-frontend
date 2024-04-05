2aed85802097e9d480697d659c08d7f581109d66
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-05 11:55:52 +0200

    Added check, cross and timer icon components

---
+++ src/components/iconComponents/crossComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="material-symbols-outlined">
+    close
+  </span>
+</template>
+
+<style scoped>
+.material-symbols-outlined {
+  color: white;
+  font-size: 40px;
+  user-select: none;
+}
+</style>
+<script setup lang="ts">
+</script>
